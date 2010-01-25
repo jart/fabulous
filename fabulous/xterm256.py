202adc9d5049d9aec692ff214e5f3e6e2075be94
J.A. Roberts Tunney <jtunney@lobstertech.com>
2010-01-25 00:20:56 -0500

    fixed color conversion algorithm

--- fabulous/xterm256.py
+++ fabulous/xterm256.py
@@ -1,7 +1,7 @@
 """Implements Support for 256-color Terminals
 """
 
-import colorsys
+from grapefruit import Color
 
 
 CUBE_STEPS = [0x00, 0x5F, 0x87, 0xAF, 0xD7, 0xFF]
@@ -11,87 +11,38 @@ BASIC16 = ((0, 0, 0), (205, 0, 0), (0, 205, 0), (205, 205, 0),
            (92, 92, 255), (255, 0, 255), (0, 255, 255), (255, 255, 255))
 
 
-def xterm_to_rgb(color):
-    if color < 16:
+def xterm_to_rgb(xcolor):
+    assert 0 <= xcolor <= 255
+    if xcolor < 16:
         # basic colors
-        return BASIC16[color]
-    elif 16 <= color <= 232:
+        return BASIC16[xcolor]
+    elif 16 <= xcolor <= 231:
         # color cube
-        color -= 16
-        return (CUBE_STEPS[(color / 36) % 6],
-                CUBE_STEPS[(color / 6) % 6],
-                CUBE_STEPS[color % 6])
-    elif 233 <= color <= 253:
+        xcolor -= 16
+        return (CUBE_STEPS[(xcolor / 36) % 6],
+                CUBE_STEPS[(xcolor / 6) % 6],
+                CUBE_STEPS[xcolor % 6])
+    elif 232 <= xcolor <= 255:
         # gray tone
-        c = 8 + (color - 232) * 0x0A
+        c = 8 + (xcolor - 232) * 0x0A
         return (c, c, c)
+COLOR_TABLE = [xterm_to_rgb(i) for i in xrange(256)]
+
+
+def rgb_to_xterm(color):
+    if hasattr(color, 'rgb'):
+        (r, g, b) = [int(round(c * 255.0)) for c in color.rgb]
+    elif isinstance(color, basestring):
+        (r, g, b) = [int(round(c * 255.0)) for c in Color.HtmlToRgb(color)]
     else:
-        assert False
-COLOR_TABLE = [xterm_to_rgb(i) for i in xrange(254)]
-
-
-def rgb_to_xterm(rgb):
-    rgb = parse_rgb(rgb)
+        (r, g, b) = color
     best_match = 0
     smallest_distance = 10000000000
-    for c in xrange(254):
-        d = (COLOR_TABLE[c][0] - rgb[0]) ** 2 + \
-            (COLOR_TABLE[c][1] - rgb[1]) ** 2 + \
-            (COLOR_TABLE[c][2] - rgb[2]) ** 2
+    for c in xrange(16, 256):
+        d = (COLOR_TABLE[c][0] - r) ** 2 + \
+            (COLOR_TABLE[c][1] - g) ** 2 + \
+            (COLOR_TABLE[c][2] - b) ** 2
         if d < smallest_distance:
             smallest_distance = d
             best_match = c
     return best_match
-
-
-def rgb_complement(rgb):
-    rgb = parse_rgb(rgb)
-
-
-def parse_rgb(color):
-    """Turns various input formats into an RGB tuple
-
-    >>> parse_rgb('#ff0000')
-    (255, 0, 0)
-    >>> parse_rgb('#f00')
-    (255, 0, 0)
-    >>> parse_rgb((255, 0, 0))
-    (255, 0, 0)
-    >>> parse_rgb((255, 0, 0, 255))
-    (255, 0, 0)
-    >>> parse_rgb((255, 0, 0, 0))
-    (0, 0, 0)
-    """
-    if isinstance(color, basestring):
-        if color.startswith('#'):
-            color = color[1:]
-        if len(color) == 3:
-            color = "".join([c * 2 for c in color])
-        return (int(color[:2], base=16),
-                int(color[2:4], base=16),
-                int(color[4:6], base=16))
-    else:
-        if len(color) == 3:
-            return color
-        elif len(color) == 4:
-            # channel 4 = alpha
-            # let's assume terminal background is black lol
-            return rgb_alpha_to_black(color)
-        else:
-            assert False
-
-
-def rgb_alpha_to_black(color):
-    assert len(color) == 4
-    if color[3] <= 0.001:
-        return (0, 0, 0)
-    elif color[3] >= 0.999:
-        return color[:3]
-    else:
-        rgba_f = [float(c) / 255.0 for c in color]
-        hsv_f = colorsys.rgb_to_hsv(*rgba_f[:3])
-        hsv_f = (hsv_f[0], hsv_f[1], hsv_f[2] * rgba_f[3])
-        rgb_f = colorsys.hsv_to_rgb(*hsv_f)
-        rgb = [int(round(c * 255.0)) for c in color]
-        return rgb
-
