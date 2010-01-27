c2878678abbb2e8cd3350a7e1b77bfa3838ed31a
J.A. Roberts Tunney <jtunney@lobstertech.com>
2010-01-27 01:22:20 -0500

    better xterm256 support with c speedup library

--- fabulous/xterm256.py
+++ fabulous/xterm256.py
@@ -1,7 +1,7 @@
 """Implements Support for 256-color Terminals
 """
 
-from grapefruit import Color
+import logging
 
 
 CUBE_STEPS = [0x00, 0x5F, 0x87, 0xAF, 0xD7, 0xFF]
@@ -26,16 +26,18 @@ def xterm_to_rgb(xcolor):
         # gray tone
         c = 8 + (xcolor - 232) * 0x0A
         return (c, c, c)
+
+
 COLOR_TABLE = [xterm_to_rgb(i) for i in xrange(256)]
+def rgb_to_xterm(r, g, b):
+    """
+    OMG there is like no easy way to optimize this!!!  Someone smarter
+    than me should read this:
 
-
-def rgb_to_xterm(color):
-    if hasattr(color, 'rgb'):
-        (r, g, b) = [int(round(c * 255.0)) for c in color.rgb]
-    elif isinstance(color, basestring):
-        (r, g, b) = [int(round(c * 255.0)) for c in Color.HtmlToRgb(color)]
-    else:
-        (r, g, b) = color
+    http://algolist.manual.ru/graphics/quant/qoverview.php
+    """
+    if r < 5 and g < 5 and b < 5:
+        return 16
     best_match = 0
     smallest_distance = 10000000000
     for c in xrange(16, 256):
@@ -46,3 +48,36 @@ def rgb_to_xterm(color):
             smallest_distance = d
             best_match = c
     return best_match
+
+
+def compile_speedup():
+    """Tries to compile/link the C version of this module
+
+    Like it really makes a huge difference.  With a little bit of luck
+    this should *just work* for you.
+
+    You need:
+
+    - Python >= 2.5 for ctypes library
+    - gcc (sudo apt-get install gcc)
+
+    """
+    import os, ctypes
+    from os.path import join, dirname, getmtime, exists
+    library = join(dirname(__file__), '_xterm256.so')
+    sauce = join(dirname(__file__), '_xterm256.c')
+    if not exists(library) or getmtime(sauce) > getmtime(library):
+        build = "gcc -fPIC -shared -o %s %s" % (library, sauce)
+        assert os.system(build) == 0
+    xterm256_c = ctypes.cdll.LoadLibrary(library)
+    xterm256_c.init()
+    def xterm_to_rgb(xcolor):
+        res = xterm256_c.rgb_to_xterm_i(xcolor)
+        return ((res >> 16) & 0xFF, (res >> 8) & 0xFF, res & 0xFF)
+    return (xterm256_c.rgb_to_xterm, xterm_to_rgb)
+
+
+try:
+    (rgb_to_xterm, xterm_to_rgb) = compile_speedup()
+except:
+    logging.exception("Failed to compile xterm256 speedup code")
