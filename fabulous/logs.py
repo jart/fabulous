380db0c3083d408e3843cea787a20229ecbab651
J.A. Roberts Tunney <jtunney@lobstertech.com>
2010-01-27 01:25:08 -0500

    did some stuff to transient logger

---
+++ fabulous/logs.py
@@ -0,0 +1,72 @@
+
+import sys
+import logging
+
+from fabulous import utils
+
+
+class TransientStreamHandler(logging.StreamHandler):
+    def __init__(self, strm=sys.stderr, level=logging.WARNING):
+        logging.StreamHandler.__init__(self, strm)
+        self.levelno = level if isinstance(level, int) else logging._levelNames[level]
+        self.need_cr = False
+        self.last = ""
+        self.parent = logging.StreamHandler
+
+    def close(self):
+        if self.need_cr:
+            self.stream.write("\n")
+            self.need_cr = False
+        self.parent.close(self)
+
+    def write(self, data):
+        if self.need_cr:
+            width = max(min(utils.term_width(), len(self.last)), len(data))
+            fmt = "\r%-" + str(width) + "s\n" + self.last
+        else:
+            fmt = "%s\n"
+        try:
+            self.stream.write(fmt % (data))
+        except UnicodeError:
+            self.stream.write(fmt % (data.encode("UTF-8")))
+
+    def transient_write(self, data):
+        if self.need_cr:
+            self.stream.write('\r')
+        else:
+            self.need_cr = True
+        width = utils.term_width()
+        for line in data.rstrip().split('\n'):
+            if line:
+                if len(line) > width:
+                    line = line[:width - 3] + '...'
+                line_width = max(min(width, len(self.last)), len(line))
+                fmt = "%-" + str(line_width) + "s"
+                self.last = line
+                try:
+                    self.stream.write(fmt % (line))
+                except UnicodeError:
+                    self.stream.write(fmt % (line.encode("UTF-8")))
+            else:
+                self.stream.write('\r')
+                self.stream.flush()
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            if record.levelno >= self.levelno:
+                self.write(msg)
+            else:
+                self.transient_write(msg)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
+
+
+def basicConfig(level=logging.WARNING, transient_level=logging.NOTSET):
+    fmt = "%(asctime)s [%(levelname)s] [%(name)s:%(lineno)d] %(message)s"
+    logging.root.setLevel(transient_level) # <--- IMPORTANT
+    hand = TransientStreamHandler(level=level)
+    hand.setFormatter(logging.Formatter(fmt))
+    logging.root.addHandler(hand)
---
+++ fabulous/test_transientlogging.py
@@ -0,0 +1,58 @@
+
+import logging
+
+from fabulous.logs import *
+from fabulous.gotham import *
+from fabulous.color import *
+
+
+def luv():
+    msg = "hello there how are you?  i love you!  sincerely compy <3 <3"
+    while True:
+        for n in range(len(msg)) + list(reversed(range(len(msg)))):
+            yield msg[:n+1]
+
+
+def bad_things():
+    yield red("godzilla attack")
+    yield bold(red("magnetic poles reverse"))
+    yield red("caffeine use criminalized")
+    yield bold(red("more horrible things happening...."))
+    yield highlight_red("THIS INFORMATION IS NOT BEING DROWNED OUT!")
+    yield bold(red("hip hip hooray"))
+
+
+def test_transientlogger():
+    import random, time
+    happy, nightmare = luv(), bad_things()
+    try:
+        while True:
+            if random.randrange(60) == 0:
+                logger.warning(nightmare.next())
+            else:
+                logger.debug(happy.next())
+            time.sleep(0.02)
+    except StopIteration:
+        pass
+
+
+def test_transientlogger2():
+    import time, random
+    gothic = lorem_gotham()
+    try:
+        while True:
+            if random.randrange(20) == 0:
+                logger.warning(red(gothic.next()))
+            else:
+                logger.debug(gothic.next())
+            time.sleep(0.1)
+    except StopIteration:
+        pass
+
+
+if __name__ == '__main__':
+    basicConfig(level=logging.WARNING)
+    logging.warning("RUNNING TEST: test_transientlogger()")
+    test_transientlogger()
+    logging.warning("RUNNING TEST: test_transientlogger2()")
+    test_transientlogger2()
