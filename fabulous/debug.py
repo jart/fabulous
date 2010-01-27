473260c9d15636067e42f46b84ba4e04efc1fec7
J.A. Roberts Tunney <jtunney@lobstertech.com>
2010-01-27 01:26:44 -0500

    way cool image library

---
+++ fabulous/debug.py
@@ -0,0 +1,27 @@
+
+import itertools
+
+from fabulous import image
+
+
+class DebugImage(image.Image):
+    def reduce(self, colors):
+        need_reset = False
+        line = ''
+        for color, items in itertools.groupby(colors):
+            if color is None:
+                if need_reset:
+                    line = line[:-1] + ">"
+                    need_reset = False
+                line += 'T' + (self.pad * len(list(items)))[1:]
+            elif color == "EOL":
+                if need_reset:
+                    line = line[:-1] + ">"
+                    need_reset = False
+                    yield line.rstrip(' T')
+                else:
+                    yield line.rstrip(' T')
+                line = ''
+            else:
+                need_reset = True
+                line += '<' + (self.pad * len(list(items)))[1:]
--- fabulous/image.py
+++ fabulous/image.py
@@ -11,25 +11,36 @@ from fabulous import utils, xterm256
 
 
 class Image(object):
+    pad = ' '
+
     def __init__(self, path, width=None, bgcolor='black'):
-        self.bgcolor = Color.NewFromHtml(bgcolor)
         self.img = Pills.open(path)
+        self.bgcolor = Color.NewFromHtml(bgcolor)
         self.resize(width)
 
     def __str__(self):
         return "\n".join(self)
 
     def __iter__(self):
-        return self.reduce(self.convert())
+        # strip out blank lines
+        for line in self.reduce(self.convert()):
+            if line.strip():
+                yield line
+        yield ""
+
+    @property
+    def size(self):
+        return self.img.size
 
     def resize(self, width=None):
-        (iw, ih) = self.img.size
+        (iw, ih) = self.size
         if width is None:
-            width = utils.term_width()
-            if iw * 2 <= width:
-                return
-        width //= 2
+            width = min(iw, utils.term.width)
+        elif isinstance(width, basestring):
+            percents = dict([(pct, '%s%%' % (pct)) for pct in range(101)])
+            width = percents[width]
         height = int(float(ih) * (float(width) / float(iw)))
+        height //= 2
         self.img = self.img.resize((width, height))
 
     def reduce(self, colors):
@@ -40,7 +51,7 @@ class Image(object):
                 if need_reset:
                     line.append("\x1b[49m")
                     need_reset = False
-                line.append('  ' * len(list(items)))
+                line.append(self.pad * len(list(items)))
             elif color == "EOL":
                 if need_reset:
                     line.append("\x1b[49m")
@@ -52,7 +63,8 @@ class Image(object):
                 line = []
             else:
                 need_reset = True
-                line.append("\x1b[48;5;%dm%s" % (color, '  ' * len(list(items))))
+                line.append("\x1b[48;5;%dm%s" % (
+                    color, self.pad * len(list(items))))
 
     def convert(self):
         (width, height) = self.img.size
@@ -63,8 +75,32 @@ class Image(object):
                 if len(rgba) == 4 and rgba[3] == 0:
                     yield None
                 elif len(rgba) == 3 or rgba[3] == 255:
-                    yield xterm256.rgb_to_xterm(rgba[:3])
+                    yield xterm256.rgb_to_xterm(*rgba[:3])
                 else:
                     color = Color.NewFromRgb(*[c / 255.0 for c in rgba])
-                    yield xterm256.rgb_to_xterm(color.AlphaBlend(self.bgcolor))
+                    rgba = color.AlphaBlend(self.bgcolor).rgb
+                    yield xterm256.rgb_to_xterm(*[int(c * 255.0) for c in rgba])
             yield "EOL"
+
+
+class BlockyImage(Image):
+    pad = '  '
+
+    def resize(self, width=None):
+        (iw, ih) = self.size
+        if width is None:
+            width = utils.term.width
+            if iw * 2 <= width:
+                return
+        elif isinstance(width, basestring):
+            percents = dict([(pct, '%s%%' % (pct)) for pct in range(101)])
+            width = percents[width]
+        width //= 2
+        height = int(float(ih) * (float(width) / float(iw)))
+        self.img = self.img.resize((width, height))
+
+
+if __name__ == '__main__':
+    for imgpath in sys.argv[1:]:
+        for line in Image(imgpath):
+            print line
