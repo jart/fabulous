e57493c6ef2c1b37ce43531255857374be146d11
J.A. Roberts Tunney <jtunney@lobstertech.com>
2010-01-27 01:26:06 -0500

    utilities and stuff

--- fabulous/utils.py
+++ fabulous/utils.py
@@ -4,7 +4,15 @@ import struct
 import termios
 
 
-def term_width():
-    call = fcntl.ioctl(0, termios.TIOCGWINSZ, "\000" * 8)
-    height, width = struct.unpack("hhhh", call)[:2]
-    return width
+class TerminalInfo(object):
+    @property
+    def dimensions(self):
+        call = fcntl.ioctl(0, termios.TIOCGWINSZ, "\000" * 8)
+        height, width = struct.unpack("hhhh", call)[:2]
+        return width, height
+
+    width = property(lambda self: self.dimensions[0])
+    height = property(lambda self: self.dimensions[1])
+
+
+term = TerminalInfo()
