ab94f22f1730170f5ecd1fc920e73ee821ce76a6
Justine Tunney <jtunney@gmail.com>
2016-07-03 13:10:31 -0400

    Fix setuptools configuration

--- setup.py
+++ setup.py
@@ -3,31 +3,33 @@
 # http://wiki.python.org/moin/CheeseShopTutorial
 # http://pypi.python.org/pypi?:action=list_classifiers
 
-from ez_setup import use_setuptools
-use_setuptools(version='0.6c11')
-
 import os
-from setuptools import setup, find_packages
+
+try:
+    import setuptools
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools(version='0.6c11')
+    import setuptools
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 version = __import__('fabulous').__version__
 
-setup(
+setuptools.setup(
     name                 = 'fabulous',
     version              = version,
     url                  = 'https://github.com/jart/fabulous',
     author               = 'Justine Tunney',
     author_email         = 'jtunney@gmail.com',
     description          = 'Makes your terminal output totally fabulous',
-    download_url         = ('https://github.com/jart/fabulous'
-                            '/releases/download/' + version +
-                            '/fabulous-' + version + '.tar.gz'),
+    download_url         = ('https://github.com/jart/fabulous/archive/' +
+                            version + '.tar.gz'),
     long_description     = read('README.rst'),
     license              = 'MIT',
     install_requires     = ['grapefruit'],
-    packages             = find_packages(),
+    packages             = ['fabulous', 'fabulous.experimental'],
     zip_safe             = False,
     include_package_data = True,
     classifiers = [
